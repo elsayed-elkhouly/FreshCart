c40daac2b43182479539a9b786928e2bb4c3f3b2
elsayed-elkhouly <seko300mo@gmail.com>
2025-09-23 20:48:42 +0300

    تعديل

--- src/apis/getallProduct.ts
+++ src/apis/getallProduct.ts
@@ -1,5 +1,5 @@
 export default async function getallProduct() {
-  const respons = await fetch("http://localhost:3000/api/Product")
+  const respons = await fetch("https://ecommerce.routemisr.com/api/v1/Product")
   const { data } = await respons.json()
   return data
 }
