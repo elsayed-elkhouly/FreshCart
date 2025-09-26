b046442d8c06caf3fa394122ebb1e0725c0a2a78
elsayed-elkhouly <seko300mo@gmail.com>
2025-09-26 06:39:33 +0300

    t

--- src/app/PaymentAction/Onlineorder.ts
+++ src/app/PaymentAction/Onlineorder.ts
@@ -3,7 +3,7 @@ import { getmytoken } from "@/utilites/token";
 import axios from "axios";
 export async function OnlineOrder(cartid:string,value:object){
    const token =await getmytoken()
-   const {data}= await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartid}?url=http://localhost:3000`,value,{
+   const {data}= await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartid}?url=https://fresh-cart-two-liart.vercel.app/`,value,{
     headers:{
         token:token as string
     }
